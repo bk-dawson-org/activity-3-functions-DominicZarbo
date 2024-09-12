4eeea4b399ec0d612df08212ddb20779096f6c11
DominicZarbo <dominic.zarbo@dawsoncollege.qc.ca>
2024-09-12 20:51:45 +0000

    cake

---
+++ cake.py
@@ -0,0 +1,22 @@
+import math
+
+radiusBottom = float(input("What is the radius of a layer of the bottom tier "))
+heightBottom = float(input("What is the height of a layer of the bottom tier "))
+radiusTop = float(input("What is the radius of a layer of the top tier "))
+heightTop = float(input("What is the height of a layer of the top tier "))
+numberOfLayers = 3
+def cakeVolume(radiusBottom, heightBottom, radiusTop, heightTop, numberOfLayers) :   
+    volumeBottom = round(((math.pi) * (radiusBottom ** 2) * (heightBottom)) * (numberOfLayers), 2)
+    volumeTop = round(((math.pi) * (radiusTop ** 2) * (heightTop)) * (numberOfLayers), 2)
+    volumeTotal = volumeBottom + volumeTop
+    return volumeTotal
+volumeTotal = str(cakeVolume(radiusBottom, heightBottom, radiusTop, heightTop, numberOfLayers))
+print("The volume of the cake is " + volumeTotal)
+
+sliceVolume = int(input("What is the desired volume of a slice of cake "))
+volumeTotalMath = cakeVolume(radiusBottom, heightBottom, radiusTop, heightBottom, numberOfLayers)
+def cakeSlice(slicevolume) :
+    numberOfSlices= int((volumeTotalMath)/(sliceVolume))
+    return numberOfSlices
+numberOfSlices = str(cakeSlice(sliceVolume))
+print("The number of slices to cut is " + numberOfSlices)
